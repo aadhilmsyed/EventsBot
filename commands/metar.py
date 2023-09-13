ca02cb82ddb0b2e221096fc34c2f8c9aaa6c3605
ThePickle <speedbird99.concorde@gmail.com>
2023-09-13 16:10:01 -0700

    Implemented Image API Logic for METAR Embed

--- commands/metar.py
+++ commands/metar.py
@@ -9,38 +9,11 @@ from bot_logger import logger
 # Import Other Necessary Libraries
 import aiohttp
 import requests
+import time
+import math
 
-async def get_metar_info(icao_code : str):
-    """
-    Description:
-        Fetches METAR information for a specified airport ICAO code.
-
-    Parameters:
-        icao_code (str): The ICAO code of the airport for which METAR information is requested.
-
-    Returns:
-        dict: A dictionary containing METAR information, or None if the data couldn't be retrieved.
-
-    Raises:
-        ValueError: If the provided ICAO code is not valid (incorrect format).
-        requests.RequestException: If there is an issue with the HTTP request.
-    """
-    
-    # API URL for METAR info
-    api_url = f"https://beta.aviationweather.gov/cgi-bin/data/metar.php?ids={icao_code}&format=json"
-    
-    try:
-        # Send a GET request to the API
-        response = requests.get(api_url)
-
-        # Return the response if the request was successful
-        if response.status_code == 200: return response.json()[0]
-        
-        # If the request was unsuccessful, return nothing
-        else: return None
-
-    # Log any Errors
-    except Exception as e: logger.error(f"An error occurred: {e}")
+# Import Necessary Local Files
+from data import MAX_REQUESTS, TIME_WINDOW, request_timestamps
 
 @bot.command()
 async def metar(ctx, icao_code : str):
@@ -56,6 +29,12 @@ async def metar(ctx, icao_code : str):
     """
     
     try:
+        # Check the Rate Limit (Terminate with Error Msg if Usage is Too High)
+        limit_exceeded, minutes, seconds = await check_rate_limit()
+        if limit_exceeded:
+            ctx.send(f"Rate Limit Exceeded. Please try again in {minutes} minutes and {seconds} seconds.")
+            return
+    
         # Perform Input Validation on the ICAO Airport Code
         if not icao_code.isalnum() or not (len(icao_code) == 4):
             await ctx.send ("Invalid ICAO code format. It must 4 alphanumeric characters.")
@@ -97,52 +76,114 @@ async def metar(ctx, icao_code : str):
             
         # Add raw METAR Info to Embed
         embed.add_field(name = "Raw METAR", value = f"{metar_data['rawOb']}")
+        
+        # Add airport picture as the thumbnail
+        image_url = await get_airport_image(f"{metar_data['name]}")
+        if image_url: embed.set_thumbnail(url = image_url)
 
         # Set Embed Footer
-        embed.set_footer(text="Data provided by Aviation Weather Center API")
+        text = "Data Provided by Aviation Weather Center API.\nImage Provided by Unsplash API."
+        embed.set_footer(text = text)
 
         await ctx.send(embed = embed)
     
     # Log any Errors
     except Exception as e: logger.error(f"An error occurred: {e}")
     
-"""
-# Sample METAR data
-metar_data = {
-    'metar_id': 416157371,
-    * 'icaoId': 'KSFO',
-    'receiptTime': '2023-09-13 21:00:18',
-    'obsTime': 1694638560,
-    * 'reportTime': '2023-09-13 21:00:00',
-    * 'temp': 22.2,
-    * 'dewp': 14.4,
-    * 'wdir': 310,
-    * 'wspd': 8,
-    'wgst': None,
-    * 'visib': '10+',
-    * 'altim': 1013,
-    'slp': 1012.9,
-    'qcField': 12,
-    'wxString': None,
-    'presTend': -1.5,
-    'maxT': None,
-    'minT': None,
-    'maxT24': None,
-    'minT24': None,
-    'precip': None,
-    'pcp3hr': None,
-    'pcp6hr': None,
-    'pcp24hr': None,
-    'snow': None,
-    'vertVis': None,
-    'metarType': 'METAR',
-    * 'rawOb': 'KSFO 132056Z 31008KT 10SM FEW006 22/14 A2991 RMK AO2 SLP129 T02220144 58015 $',
-    'mostRecent': 1,
-    'lat': 37.62,
-    'lon': -122.366,
-    'elev': 2,
-    'prior': 1,
-    * 'name': 'San Francisco Intl, CA, US',
-    * 'clouds': [{'cover': 'FEW', 'base': 600}]
-}
-"""
+    
+async def get_metar_info(icao_code : str):
+    """
+    Description:
+        Fetches METAR information for a specified airport ICAO code.
+
+    Parameters:
+        icao_code (str): The ICAO code of the airport for which METAR information is requested.
+
+    Returns:
+        dict: A dictionary containing METAR information, or None if the data couldn't be retrieved.
+
+    Raises:
+        ValueError: If the provided ICAO code is not valid (incorrect format).
+        requests.RequestException: If there is an issue with the HTTP request.
+    """
+    
+    # API URL for METAR info
+    api_url = f"https://beta.aviationweather.gov/cgi-bin/data/metar.php?ids={icao_code}&format=json"
+    
+    try:
+        # Send a GET request to the API
+        response = requests.get(api_url)
+
+        # Return the response if the request was successful
+        if response.status_code == 200: return response.json()[0]
+        
+        # If the request was unsuccessful, return nothing
+        else: return None
+
+    # Log any Errors
+    except Exception as e: logger.error(f"An error occurred: {e}")
+
+
+# Function to fetch an image of an airport using its ICAO code
+async def get_airport_image(icao_code : str):
+
+    # Set up your Unsplash API credentials (replace with your own API key)
+    api_key = 'S4v0a9mxDRksIXG1GsuVkh28PwXfE7kptHEqQGHqKhE'
+
+    # Define the query for the airport using its ICAO code
+    search_query = f"{icao_code} airport"
+
+    # Construct the URL for the Unsplash API search
+    url = f"https://api.unsplash.com/search/photos/?query={search_query}&client_id={api_key}"
+
+    try:
+        # Send a GET request to the Unsplash API
+        response = requests.get(url)
+
+        # Check if the request was successful (status code 200)
+        if response.status_code == 200:
+            data = response.json()
+
+            # Check if there are any results
+            if 'results' in data and len(data['results']) > 0:
+                # Get the URL of the first image in the results
+                image_url = data['results'][0]['urls']['regular']
+                return image_url
+
+    # Log any Errors
+    except Exception as e: logger.error(f"An error occurred: {e}")
+
+    # Return None if no image was found
+    return None
+
+import time
+
+# Function to check rate limit and record request timestamps
+async def check_rate_limit():
+    
+    # Store the Current Time
+    current_time = time.time()
+
+    # Remove timestamps that are older than the time window
+    request_timestamps[:] = [timestamp for timestamp in request_timestamps if current_time - timestamp <= TIME_WINDOW]
+    
+    # Define minutes and seconds variables
+    minutes, seconds = 0, 0
+
+  # Check if the rate limit is exceeded
+    if len(request_timestamps) >= MAX_REQUESTS:
+    
+        # Calculate remaining time
+        remaining_time = int((request_timestamps[user_id][0] + TIME_WINDOW) - current_time)
+
+        # Calculate minutes and seconds
+        minutes, seconds = divmod(remaining_time, 60)
+    
+        # Return Rate Limit Exceeded
+        return True, minutes, seconds
+
+    # Record the current request timestamp
+    request_timestamps.append(current_time)
+    
+    # Return Rate Limit Not Exceeded
+    return False, minutes, seconds
