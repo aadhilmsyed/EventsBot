fc6c02783638b44892320c4db7ec365ce1b2a558
ThePickle <speedbird99.concorde@gmail.com>
2023-09-13 13:46:59 -0700

    Reconfigured Files to Original Mapping

---
+++ data.py
@@ -0,0 +1,15 @@
+# Dictionary of Ban/Kick/Timeout Reasons
+ban_reasons     = {}
+kick_reasons    = {}
+timeout_reasons = {}
+
+# List of restricted announcement channels
+restricted_channels = []
+
+# Log Channels
+member_logs_channel  = None
+message_logs_channel = None
+mod_logs_channel     = None
+server_logs_channel  = None
+voice_logs_channel   = None
+event_logs_channel   = None
