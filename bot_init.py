1343364922a9474d32d47f837f4ef8af01a447db
Aadhil Syed <speedbird99.concorde@gmail.com>
2023-10-02 21:28:36 -0700

    Added .gitignore file to protect important information

--- bot_init.py
+++ bot_init.py
@@ -2,6 +2,9 @@
 import discord
 from discord.ext import commands
 
+# Token to Connect to Discord API
+TOKEN = 'MTE0OTUwNjI2MTExMzY0MzA4Mw.GyxGyO.xyVPFsGrMOz_izZ-0aRKNYBb0obznJsjdn1qIE'
+
 # Define Intents & Create Bot Object
 intents = discord.Intents.all()
 bot = commands.Bot(command_prefix = '!', intents = intents)
