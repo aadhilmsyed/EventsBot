05dca594f0dc8aad3d80f1e62bb184c513f80a2d
Aadhil Syed <speedbird99.concorde@gmail.com>
2023-10-03 16:16:43 -0700

    Added Untracked Files to Commit

---
+++ config.py
@@ -0,0 +1,17 @@
+# List of restricted announcement channels
+restricted_channels = []
+
+# Dictionary of Flight Hours
+flight_hours = {}
+
+# Dictionary to Store Amount of Time in Voice Channel
+start_time = {}
+
+# Flag to Indicate whether an Event is active or not
+is_event_active = False
+
+# Voice Channel of the Event
+voice_channel = None
+
+# Embed Thumbnail for METAR commands
+metar_embed_thumbnail_url = "https://media.istockphoto.com/id/537337166/photo/air-trafic-control-tower-and-airplance-at-paris-airport.jpg?b=1&s=612x612&w=0&k=20&c=kp14V8AXFNUh5jOy3xPQ_sxhOZLWXycdBL-eUGviMOQ="
