9090878f6476c0379ab6eb8a141e71d3d76d0e06
Aadhil Syed <speedbird99.concorde@gmail.com>
2023-10-02 22:47:49 -0700

    Reconfigured File Structure

--- commands/mod_commands.py
+++ commands/mod_commands.py
@@ -7,9 +7,7 @@ from bot_init import bot
 from bot_logger import logger
 
 # Import Necessary Local Files
-from logs.message_logs import log_purged_messages
-from data              import ban_reasons, kick_reasons, timeout_reasons
-from data              import restricted_channels
+from data import restricted_channels
 
 @bot.command()
 @commands.has_permissions(administrator = True)
@@ -50,46 +48,3 @@ async def remove_restricted_channel(ctx, channel: discord.TextChannel):
         await ctx.send(f"{channel.mention} is no longer a restricted announcement channel.")
     else:
         await ctx.send(f"{channel.mention} is not a restricted announcement channel.")
-        
-# TODO: Implement Kick, Ban, Timeout Functions
-# TODO: Implement kick_reasons, ban_reasons, timeout_reasons dictionaries
-# TODO: Save all dictionaries to a database in case bot fails
-
-@bot.command()
-@commands.has_permissions(manage_messages=True)
-async def purge(ctx, limit: int):
-    """
-    Description:
-        Command to purge a specified number of messages in a channel.
-
-    Parameters:
-        ctx (discord.ext.commands.Context): The context object representing the command's context.
-        limit (int): The number of messages to purge.
-
-    Returns:
-        None
-    """
-    
-    # Convert the Argument to Integer, throw Error if not an integer
-    try: limit = int(limit)
-    except ValueError:
-        await ctx.send("Please specify an integer value between 1 and 500.")
-        return
-    
-    # Check if the user provided a valid limit
-    if limit <= 0 or limit > 500:
-        await ctx.send("Please specify an integer value between 1 and 500.")
-        return
-
-    # Purge messages (limit + 1 to purge command message as well)
-    try: purged_messages = await ctx.channel.purge(limit + 1)
-    
-    # Log any Errors
-    except Exception as e: logger.error(f"An error occurred: {e}")
-    
-    # Log the Purged Messages
-    try:
-        await log_purged_messages(purged_messages, ctx.author.mention)
-        
-    # Log any Errors
-    except Exception as e: logger.error(f"An error occurred: {e}")
