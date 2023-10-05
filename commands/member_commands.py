b4838da2d6824af4f82c82bb531ef5ef654e9ae7
Aadhil Syed <speedbird99.concorde@gmail.com>
2023-10-05 15:46:14 -0700

    Added help module for members

--- commands/member_commands.py
+++ commands/member_commands.py
@@ -9,7 +9,6 @@ from bot.logger.init import logger
 # Import Necessary Local Files
 from config import flight_hours
     
-    
 def calculate_hours(flight_time):
     
     hours, remainder = divmod(flight_time, 3600)
@@ -205,70 +204,3 @@ async def ping(ctx):
     # Send the latency as a message
     await ctx.send(f'Pong! Latency is {latency} ms')
 
-@bot.command()
-async def help(ctx):
-    """
-    Description
-        Responds with an embed describing commands that a user can issue.
-
-    Parameters:
-        ctx (discord.ext.commands.Context): The context object representing the command's context.
-
-    Returns:
-        None
-    """
-    try:
-    
-        # Update Logger Info
-        logger.info(f"'ping' command was issued by {ctx.author}")
-        
-        # Create an embed
-        embed = discord.Embed(
-            title = f"GeoFS Events CoPilot",
-            color = discord.Color.blue(),
-            description = "Your Gateway to an Enhanced Flight Simulation Experience"
-        )
-        
-        # Add the Member Commands
-        embed.add_field(
-            name = "!metar",
-            value = "Retrieve the weather information for any airport with its ICAO code. Example Usage: !metar KSFO",
-            inline = False
-        )
-        embed.add_field(
-            name = "!ping",
-            value = "Returns the latency. Example Usage: !ping",
-            inline = False
-        )
-        embed.add_field(
-            name = "!metar",
-            value = "Spams a specified number of dots (1-20). If no number is specified, then it will dotspam 10 times. Example Usage: !dotspam 15",
-            inline = False
-        )
-        embed.add_field(
-            name = "!flighttime",
-            value = "Retrieve the amount of flight hours you have logged. Example Usage: !flighttime",
-            inline = False
-        )
-        embed.add_field(
-            name = "!leaderboard",
-            value = "Checkout who has the most flight hours logged for the current month. Example Usage: !leaderboard",
-            inline = False
-        )
-        embed.add_field(
-            name = "Event Participation.",
-            value = "By participating in our events, you can increase your flight hours. At the end of each month, roles will be udpated to reflect flight activity, so make sure to join us whenever you can. We are thrilled to have you here at GeoFS Events, and we hope you enjoy your time here. :)",
-            inline = False
-        )
-        
-        # Set the server logo as the embed thumbnail
-        embed.set_thumbnail(url = ctx.guild.icon.url)
-        
-        # Set footnote
-        embed.set_footer(text = "Developed Proudly by GeoFS Flights Channel (the._.pickle)")
-        
-        # Send the latency as a message
-        await ctx.send(embed = embed)
-        
-    # Log any Errors
-    except Exception as e: logger.error(e)
