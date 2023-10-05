06bf8274e094cdfc9c0ddf55faada0e65790630a
Aadhil Syed <speedbird99.concorde@gmail.com>
2023-10-05 12:32:41 -0700

    Renamed events_bot.py to main.py

--- tests/test_events_bot.py
+++ tests/test_events_bot.py
@@ -4,7 +4,7 @@ import subprocess
 class TestEventsBot(unittest.TestCase):
     def test_run_events_bot(self):
         # Use subprocess to run events_bot.py
-        result = subprocess.run(["python", "events_bot.py"], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+        result = subprocess.run(["python", "main.py"], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
 
         # Check if the process completed without errors
         self.assertEqual(result.returncode, 0, f"Error: {result.stderr}")
