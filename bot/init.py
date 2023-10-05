c8864af7bcfb7cdd53f98e032fac9b7edeb3fc9c
Aadhil Syed <speedbird99.concorde@gmail.com>
2023-10-05 15:58:48 -0700

    Restructured help module

--- bot/init.py
+++ bot/init.py
@@ -4,4 +4,7 @@ from discord.ext import commands
 
 # Define Intents & Create Bot Object
 intents = discord.Intents.all()
-bot = commands.Bot(command_prefix = '!', intents = intents)
+bot = commands.Bot(command_prefix = '!', intents = intents, help_command = None)
+
+# Remove the help command
+bot.remove_command('help')
--- commands/help.py
+++ commands/help.py
@@ -6,78 +6,71 @@ from discord.ext import commands
 from bot.init import bot
 from bot.logger.init import logger
 
-# Define a custom help command by subclassing DefaultHelpCommand
-class MyHelpCommand(commands.DefaultHelpCommand):
-    def __init__(self):
-        super().__init__()
 
-    # Override the send_help method to customize the help message
-    async def send_help(self, ctx):
-        """
-        Description
-            Responds with an embed describing commands that a user can issue.
+@bot.command()
+async def send_help(ctx):
+    """
+    Description
+        Responds with an embed describing commands that a user can issue.
 
-        Parameters:
-            ctx (discord.ext.commands.Context): The context object representing the command's context.
+    Parameters:
+        ctx (discord.ext.commands.Context): The context object representing the command's context.
 
-        Returns:
-            None
-        """
-        try:
+    Returns:
+        None
+    """
+    try:
+    
+        # Update Logger Info
+        logger.info(f"'ping' command was issued by {ctx.author}")
         
-            # Update Logger Info
-            logger.info(f"'ping' command was issued by {ctx.author}")
-            
-            # Create an embed
-            embed = discord.Embed(
-                title = f"GeoFS Events CoPilot",
-                color = discord.Color.blue(),
-                description = "Your Gateway to an Enhanced Flight Simulation Experience"
-            )
-            
-            # Add the Member Commands
-            embed.add_field(
-                name = "!metar",
-                value = "Retrieve the weather information for any airport with its ICAO code. Example Usage: !metar KSFO",
-                inline = False
-            )
-            embed.add_field(
-                name = "!ping",
-                value = "Returns the latency. Example Usage: !ping",
-                inline = False
-            )
-            embed.add_field(
-                name = "!metar",
-                value = "Spams a specified number of dots (1-20). If no number is specified, then it will dotspam 10 times. Example Usage: !dotspam 15",
-                inline = False
-            )
-            embed.add_field(
-                name = "!flighttime",
-                value = "Retrieve the amount of flight hours you have logged. Example Usage: !flighttime",
-                inline = False
-            )
-            embed.add_field(
-                name = "!leaderboard",
-                value = "Checkout who has the most flight hours logged for the current month. Example Usage: !leaderboard",
-                inline = False
-            )
-            embed.add_field(
-                name = "Event Participation.",
-                value = "By participating in our events, you can increase your flight hours. At the end of each month, roles will be udpated to reflect flight activity, so make sure to join us whenever you can. We are thrilled to have you here at GeoFS Events, and we hope you enjoy your time here. :)",
-                inline = False
-            )
-            
-            # Set the server logo as the embed thumbnail
-            embed.set_thumbnail(url = ctx.guild.icon.url)
-            
-            # Set footnote
-            embed.set_footer(text = "Developed Proudly by GeoFS Flights Channel (the._.pickle)")
-            
-            # Send the latency as a message
-            await ctx.send(embed = embed)
-            
-        # Log any Errors
-        except Exception as e: logger.error(e)
-
-# Set the bot help command to my class
-bot.help_command = MyHelpCommand()
+        # Create an embed
+        embed = discord.Embed(
+            title = f"GeoFS Events CoPilot",
+            color = discord.Color.blue(),
+            description = "Your Gateway to an Enhanced Flight Simulation Experience"
+        )
+        
+        # Add the Member Commands
+        embed.add_field(
+            name = "!metar",
+            value = "Retrieve the weather information for any airport with its ICAO code. Example Usage: !metar KSFO",
+            inline = False
+        )
+        embed.add_field(
+            name = "!ping",
+            value = "Returns the latency. Example Usage: !ping",
+            inline = False
+        )
+        embed.add_field(
+            name = "!metar",
+            value = "Spams a specified number of dots (1-20). If no number is specified, then it will dotspam 10 times. Example Usage: !dotspam 15",
+            inline = False
+        )
+        embed.add_field(
+            name = "!flighttime",
+            value = "Retrieve the amount of flight hours you have logged. Example Usage: !flighttime",
+            inline = False
+        )
+        embed.add_field(
+            name = "!leaderboard",
+            value = "Checkout who has the most flight hours logged for the current month. Example Usage: !leaderboard",
+            inline = False
+        )
+        embed.add_field(
+            name = "Event Participation.",
+            value = "By participating in our events, you can increase your flight hours. At the end of each month, roles will be udpated to reflect flight activity, so make sure to join us whenever you can. We are thrilled to have you here at GeoFS Events, and we hope you enjoy your time here. :)",
+            inline = False
+        )
+        
+        # Set the server logo as the embed thumbnail
+        embed.set_thumbnail(url = ctx.guild.icon.url)
+        
+        # Set footnote
+        embed.set_footer(text = "Developed Proudly by GeoFS Flights Channel (the._.pickle)")
+        
+        # Send the latency as a message
+        await ctx.send(embed = embed)
+        
+    # Log any Errors
+    except Exception as e: logger.error(e)
