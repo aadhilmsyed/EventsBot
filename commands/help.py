94a4f239715c26649fd9b6b5c925ffe44056aedc
Aadhil Syed <aadhil.syed@seeloz.com>
2024-01-05 01:19:45 -0800

    Updated Help Module For Member Commands

--- commands/help.py
+++ commands/help.py
@@ -3,9 +3,8 @@ import discord
 from discord.ext import commands
 
 # Import Bot & Logger Objects
-from bot.init import bot
-from bot.logger.init import logger
-
+from bot import bot
+from logger import logInfo
 
 @bot.command()
 async def help(ctx):
@@ -19,58 +18,51 @@ async def help(ctx):
     Returns:
         None
     """
-    try:
+        
+    # Create an embed
+    embed = discord.Embed(
+        title = f"GeoFS Events CoPilot",
+        color = discord.Color.blue(),
+        description = "Your Gateway to an Enhanced Flight Simulation Experience"
+    )
     
-        # Update Logger Info
-        logger.info(f"'ping' command was issued by {ctx.author}")
-        
-        # Create an embed
-        embed = discord.Embed(
-            title = f"GeoFS Events CoPilot",
-            color = discord.Color.blue(),
-            description = "Your Gateway to an Enhanced Flight Simulation Experience"
-        )
-        
-        # Add the Member Commands
-        embed.add_field(
-            name = "!metar",
-            value = "Retrieve the weather information for any airport with its ICAO code. Example Usage: !metar KSFO",
-            inline = False
-        )
-        embed.add_field(
-            name = "!ping",
-            value = "Returns the latency. Example Usage: !ping",
-            inline = False
-        )
-        embed.add_field(
-            name = "!dotspam",
-            value = "Spams a specified number of dots (1-20). If no number is specified, then it will dotspam 10 times. Example Usage: !dotspam 15",
-            inline = False
-        )
-        embed.add_field(
-            name = "!flighttime",
-            value = "Retrieve the amount of flight hours you have logged. Example Usage: !flighttime",
-            inline = False
-        )
-        embed.add_field(
-            name = "!leaderboard",
-            value = "Checkout who has the most flight hours logged for the current month. Example Usage: !leaderboard",
-            inline = False
-        )
-        embed.add_field(
-            name = "Event Participation.",
-            value = "By participating in our events, you can increase your flight hours. At the end of each month, roles will be udpated to reflect flight activity, so make sure to join us whenever you can. We are thrilled to have you here at GeoFS Events, and we hope you enjoy your time here. :)",
-            inline = False
-        )
-        
-        # Set the server logo as the embed thumbnail
-        embed.set_thumbnail(url = ctx.guild.icon.url)
-        
-        # Set footnote
-        embed.set_footer(text = "Developed Proudly by GeoFS Flights Channel (the._.pickle)")
-        
-        # Send the latency as a message
-        await ctx.send(embed = embed)
-        
-    # Log any Errors
-    except Exception as e: logger.error(e)
+    # Add the Member Commands
+    embed.add_field(
+        name = "!metar",
+        value = "Retrieve the weather information for any airport with its ICAO code. Example Usage: !metar KSFO",
+        inline = False
+    )
+    embed.add_field(
+        name = "!ping",
+        value = "Returns the latency. Example Usage: !ping",
+        inline = False
+    )
+    embed.add_field(
+        name = "!dotspam",
+        value = "Spams a specified number of dots (1-20). If no number is specified, then it will dotspam 10 times. Example Usage: !dotspam 15",
+        inline = False
+    )
+    embed.add_field(
+        name = "!flighttime",
+        value = "Retrieve the amount of flight hours you have logged. Example Usage: !flighttime",
+        inline = False
+    )
+    embed.add_field(
+        name = "!leaderboard",
+        value = "Checkout who has the most flight hours logged for the current month. Example Usage: !leaderboard",
+        inline = False
+    )
+    embed.add_field(
+        name = "Event Participation.",
+        value = "By participating in our events, you can increase your flight hours. At the end of each month, roles will be udpated to reflect flight activity, so make sure to join us whenever you can. We are thrilled to have you here at GeoFS Events, and we hope you enjoy your time here. :)",
+        inline = False
+    )
+    
+    # Set the server logo as the embed thumbnail
+    embed.set_thumbnail(url = ctx.guild.icon.url)
+    
+    # Set footnote
+    embed.set_footer(text = "Developed Proudly by GeoFS Flights Channel (the._.pickle)")
+    
+    # Send the latency as a message
+    await ctx.send(embed = embed)
