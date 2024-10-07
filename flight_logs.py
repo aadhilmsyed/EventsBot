42b30839a4da244bdce1b017100adbd30b3398ff
Aadhil M. Syed <aadhilmsyed839@gmail.com>
2024-10-07 01:47:47 -0700

    Minor Edge Case Fix for No Event Voice Channel

--- flight_logs.py
+++ flight_logs.py
@@ -126,7 +126,7 @@ async def on_scheduled_event_update(before, after):
             elapsed_minutes = flight_hours_manager.log_end_time(str(member_id))
             
             # Update the logger information to the log channel
-            await logger.info(f"<@{member_id}> left {before.channel.mention if before.channel else 'the event'}. Ending Logging...")
+            await logger.info(f"<@{member_id}> left {config.guild.fetch_member(member_id).voice.channel}. Ending Logging...")
             await logger.info(f"{int(elapsed_minutes)} minutes of flight time were added to <@{member_id}>. " \
                               f"<@{member_id}> has a total flight time of {int(flight_hours_manager.flight_hours[str(member_id)])} minutes.")
             
